bedd5ee90b8aae78fc89e9f07259620300175caa
Rob Armitage <robert.c.armitage@gmail.com>
2020-09-03 20:44:48 +0100

    basic Context files

---
+++ src/context/library/LibraryState.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LibraryContext from "./libraryContext";
+
+const LibraryState = () => {
+  const initialState = {
+    fictionBooks: [],
+    nonFictionBooks: [],
+  };
+
+  return <div></div>;
+};
+import LibraryContext from "./libraryContext";
+
+export default LibraryState;
