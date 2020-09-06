bd9d1b51bf60b77c9ec232434c21ca60396e8a19
Rob Armitage <robert.c.armitage@gmail.com>
2020-09-06 01:06:57 +0100

    small layout changes

--- src/components/desk/Desk.js
+++ src/components/desk/Desk.js
@@ -5,8 +5,10 @@ const Desk = () => {
     <div className="desk">
       <h3>Desk</h3>
       <p>[Current book goes here]</p>
+      <h4 className="cover-image-desk">[Cover Image]</h4>
       <h4>Title</h4>
-      <h5>Author | Year | Fiction/Non-fiction</h5>
+      <h5>Author | Year</h5>
+      <h5>About: [About paragraph]</h5>
       <button>Link to Google Books entry</button>
       <button>Add to/remove from shelf</button>
     </div>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -7,11 +7,18 @@ const Search = () => {
       <form>
         <input type="text" placeholder="Search for title, author..." />
       </form>
-      <div className="grid-2">
-        <p>[Results listed here]</p>
-        <button>
-          View book details (sets book as current, displays it on desk)
-        </button>
+      <div>
+        <div className="results-headers">
+          <p>Title</p>
+          <p>Author</p>
+          <p>Published</p>
+        </div>
+        <div className="results-items">
+          <p>[Results listed here]</p>
+          <button>
+            View book details (sets book as current, displays it on desk)
+          </button>
+        </div>
       </div>
     </div>
   );
