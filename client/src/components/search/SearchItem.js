4a6012e202caf025d513616b5326bb32659bf614
Rob Armitage <robert.c.armitage@gmail.com>
2020-09-30 23:56:50 +0100

    searchResult as props and destructure below

--- client/src/components/search/SearchItem.js
+++ client/src/components/search/SearchItem.js
@@ -1,21 +1,26 @@
 import React, { useContext } from "react";
 import LibraryContext from "../../context/library/libraryContext";
 
-const SearchItem = ({
-  searchResult: {
-    id,
-    volumeInfo: { title, authors, publishedDate },
-  },
-}) => {
+const SearchItem = ({ searchResult }) => {
   const libraryContext = useContext(LibraryContext);
   const { setCurrent } = libraryContext;
+  const {
+    id,
+    volumeInfo: {
+      title,
+      authors,
+      publishedDate,
+      description,
+      imageLinks: { smallThumbnail },
+    },
+  } = searchResult;
 
   return (
     <div>
       <p>
         {title} | {authors} | {publishedDate}
       </p>
-      <button onClick={() => setCurrent(id)}>View Details</button>
+      <button onClick={() => setCurrent(searchResult)}>View Details</button>
     </div>
   );
 };
