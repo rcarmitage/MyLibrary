9b60fa64dbdd2daac0a449087b53698b1bda32aa
Rob Armitage <robert.c.armitage@gmail.com>
2020-12-02 22:40:19 +0000

    CSS - Navbar title + button positioning

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => (
   <div id="navbar">
-    <div id="title">
-      <h1>My Library</h1>
-    </div>
     <div className="nav-links">
       <button className="nav-button">
         <Link to="/">Home</Link>
       </button>
       <button className="nav-button">
         <Link to="/about">About</Link>
-      </button>
+      </button>{" "}
+    </div>
+    <div id="title">
+      <h1>My Library</h1>
     </div>
   </div>
 );
