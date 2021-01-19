17ad55383723128222a69041b20ebfa5e14deef8
Rob Armitage <robert.c.armitage@gmail.com>
2021-01-19 00:06:06 +0000

    UI - spinner on shelfBooks, still doesn't stop

--- client/src/components/shelf/ShelfBooks.js
+++ client/src/components/shelf/ShelfBooks.js
@@ -13,19 +13,33 @@ const ShelfBooks = () => {
     // eslint-disable-next-line
   }, [libraryContext]);
 
-  return (
-    <Fragment>
-      {shelfBooks === null ? (
-        <p>Your shelf is empty...</p>
-      ) : loading ? (
-        <Spinner />
-      ) : (
-        shelfBooks.map((book) => (
+  // return (
+  //   <Fragment>
+  //     {shelfBooks === null ? (
+  //       <p>Your shelf is empty...</p>
+  //     ) : loading ? (
+  //       <Spinner />
+  //     ) : (
+  //       shelfBooks.map((book) => (
+  //         <ShelfBookItem key={book._id} book={book} className="shelfbookitem" />
+  //       ))
+  //     )}
+  //   </Fragment>
+  // );
+
+  if (shelfBooks === null) {
+    return <p>Your shelf is empty...</p>;
+  } else if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <Fragment>
+        {shelfBooks.map((book) => (
           <ShelfBookItem key={book._id} book={book} className="shelfbookitem" />
-        ))
-      )}
-    </Fragment>
-  );
+        ))}
+      </Fragment>
+    );
+  }
 };
 
 export default ShelfBooks;
